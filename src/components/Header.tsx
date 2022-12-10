3848909785f7edba19bc80f290ed6ea7f82183e8
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-12-09 21:29:43 -0300

    dev: Adiciona logo e cores no texto do Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import rocketSvg from '../assets/rocket.svg'
 import styles from './Header.module.css'
 
 export function Header() {
   return (
     <header className={styles['header-app']}>
-      <h1 className={styles.title}>todo</h1>
+      <h1 className={styles.title}>
+        <img className={styles.logoSvg} src={rocketSvg} alt="logo" />
+        <span className={styles['title-to']}>to</span><span className={styles['title-do']}>do</span>
+      </h1>
     </header >
   )
 }
